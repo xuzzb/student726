23d915d28b2d918a50344e3ab196ab772e7ec0bd
xuzzb <xzz_thh@163.com>
2023-01-05 22:11:46 +0800

    这是726测试代码提交

---
+++ test726/src/main/java/com/dcits/test/Test.java
@@ -0,0 +1,11 @@
+package com.dcits.test;
+
+/**
+ * @Author xuzzb
+ * @Create 2023/1/5
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
